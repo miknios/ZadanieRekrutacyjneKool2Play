140d57c54c28747fcee226bce626d4b14b086637
miknios <miknios@gmail.com>
2020-09-14 19:22:09 +0200

    Add camera following

---
+++ Assets/Scripts/CameraTargetFollower.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace DefaultNamespace
+{
+	public class CameraTargetFollower : MonoBehaviour
+	{
+		[SerializeField] private Transform targetTransform;
+		[SerializeField] private float distance = 10;
+		
+		private Vector3 _fromTargetTranslation;
+		
+		private void Start()
+		{
+			_fromTargetTranslation = -transform.forward * distance;
+		}
+
+		private void FixedUpdate()
+		{
+			transform.position = targetTransform.position + _fromTargetTranslation;
+		}
+	}
+}
