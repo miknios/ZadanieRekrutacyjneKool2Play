cde027e92e7cd61167928ac3520ea55293d1e6c6
miknios <miknios@gmail.com>
2020-09-15 20:50:25 +0200

    Add health bar flash

--- Assets/Scripts/HealthAndDamage/DamageDealtAnimator.cs
+++ Assets/Scripts/HealthAndDamage/DamageDealtAnimator.cs
@@ -8,14 +8,19 @@ namespace HealthAndDamage
 	[RequireComponent(typeof(HealthComponent))]
 	public class DamageDealtAnimator : MonoBehaviour, ISpawnInitializable
 	{
+		[SerializeField] private Renderer _renderer;
+		[SerializeField] private float duration;
+		
 		private HealthComponent _healthComponent;
 		private int _previousValue;
 		private Vector3 _initialScale;
+		private Color _initialColor;
 
 		private void Awake()
 		{
 			_healthComponent = GetComponent<HealthComponent>();
 			_initialScale = transform.localScale;
+			_initialColor = _renderer.material.color;
 		}
 
 		private void Start()
@@ -33,9 +38,23 @@ namespace HealthAndDamage
 
 		private void Animate()
 		{
-			transform.DOShakeScale(0.1f, 0.6f, 5, 40f)
+			Bounce();
+			Flash();
+		}
+
+		private void Bounce()
+		{
+			transform.DOShakeScale(duration, 0.6f, 5, 40f)
 				.OnStart(() => transform.localScale = _initialScale)
 				.OnComplete(() => transform.localScale = _initialScale);
+		}
+
+		private void Flash()
+		{
+			duration = 0.1f;
+			_renderer.material.DOColor(Color.yellow, duration)
+				.From(_initialColor)
+				.SetLoops(2, LoopType.Yoyo);
 		}
 
 		public void InitializeOnSpawn()
--- Assets/Scripts/UI/HealthIndicator/HealthBarIndicator.cs
+++ Assets/Scripts/UI/HealthIndicator/HealthBarIndicator.cs
@@ -1,4 +1,5 @@
-﻿using DG.Tweening;
+﻿using System;
+using DG.Tweening;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -11,14 +12,28 @@ namespace UI.HealthIndicator
 	
 		[Header("Parameters")] 
 		[SerializeField] private float healthBarUpdateDuration = 0.3f;
+		[SerializeField] private float flashDuration = 0.1f;
+
+		private Color initialColor;
+
+		private void Awake()
+		{
+			initialColor = healthBarImage.color;
+		}
 
 		protected override void UpdateIndicator(int currentHealth, int maxHealth)
 		{
 			if(maxHealth == 0)
 				return;
 		
-			float fillValue = (float)currentHealth / maxHealth;
-			healthBarImage.DOFillAmount(fillValue, healthBarUpdateDuration);
+			float targetValue = (float)currentHealth / maxHealth;
+			float current = healthBarImage.fillAmount;
+
+			if (current > targetValue)
+				healthBarImage.DOColor(Color.yellow, flashDuration)
+					.From(initialColor)
+					.SetLoops(2, LoopType.Yoyo);
+			healthBarImage.DOFillAmount(targetValue, healthBarUpdateDuration);
 		}
 	}
 }
